9f737fe17cd154a1cff5d1b9391c98d63b7e4932
Levi Henrique <83926172+levi985@users.noreply.github.com>
2024-10-09 23:00:16 -0300

    Conclusão da etapa 3

--- menu.py
+++ menu.py
@@ -1,49 +1,40 @@
 usuarios = []
 while True:
-    menu = input("Digite o que deseja fazer. 1-Cadastro, 2-Mostrar, 3-Login ou 4-Sair: ")
-    
-    if menu == '1':
+    m = input("Digite o que deseja fazer. 1-Cadastro, 2-Mostrar, 3-Login ou 4-Sair: ")
+    if m == '1':
         nome = input("Digite seu nome: ")
         senha = input("Digite sua senha: ")
-        ja_existe = False
-
+        ex = False
         for usuario in usuarios:
             if usuario[0] == nome:
-                ja_existe = True
+                ex = True
                 break
-
-        if ja_existe:
+        if ex:
             print("Usuário já cadastrado.")
         else:
             usuarios = usuarios + [(nome, senha)]
             print(f"Usuário {nome} cadastrado com sucesso!")
-
-    elif menu == '2':
+    elif m == '2':
         if usuarios:
             print("Usuários cadastrados:")
             for usuario in usuarios:
                 print(f"- {usuario[0]}")
         else:
             print("Nenhum usuário cadastrado.")
-
-    elif menu == '3':
+    elif m == '3':
         nome = input("Digite seu nome: ")
         senha = input("Digite sua senha: ")
-        login_sucesso = False
-
+        ls = False
         for usuario in usuarios:
             if usuario[0] == nome and usuario[1] == senha:
-                login_sucesso = True
+                ls = True
                 break
-
-        if login_sucesso:
+        if ls:
             print(f"Login bem-sucedido! Bem-vindo, {nome}.")
         else:
             print("Usuário ou senha incorretos.")
-
-    elif menu == '4':
+    elif m == '4':
         print("Fim do programa")
         break
-
     else:
         print("Opção inválida. Tente novamente.")
